2227257465ddcfe67ce68126c3738d4f6188ad90
beksultan <turgunbaaev@gmail.com>
2021-05-29 23:09:54 +0600

    Added allowed host

--- ksucta/settings.py
+++ ksucta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'e=qf1%j(3a#8)36aehrrqycy%d_*1*8tlcbtsz1adf6agsiplx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ksuctainit.herokuapp.com']
 
 
 # Application definition
